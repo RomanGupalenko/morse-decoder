105773c6b6bab3fddc2a3d9cb51ea6419c41beb7
romangupalenko <gupalenko.98@mail.ru>
2020-09-30 11:20:49 +0300

    feat: add task solution

--- src/index.js
+++ src/index.js
@@ -37,8 +37,29 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const dotOrDashes = (substr) => substr === "11" ? "-" : substr === "10" ? "." : "";
+
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    let preResult = "";
+    for (let i = 0; i < (expr.length / 10); i++)
+    {
+        let sliceStr = expr.slice(i * 10, (i + 1) * 10);
+        let checksStars = /[*]{10}/.test(sliceStr);
+        if (checksStars) {
+            result += " ";
+        }
+        else 
+        {
+            for (let j = 0; j < (sliceStr.length / 2); j++)
+            {
+                preResult += dotOrDashes(sliceStr.slice(j * 2, (j + 1) * 2));
+            }
+            result += MORSE_TABLE[preResult];
+            preResult = "";
+        }
+    }
+    return result;
 }
 
 module.exports = {
